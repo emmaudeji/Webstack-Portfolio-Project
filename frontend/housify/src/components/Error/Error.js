4b44ac34bee880d14a6ea496d9f88248a5e6006b
Zelipha <zeliphawambui756@gmail.com>
2022-08-31 00:25:40 +0300

    Little progress on the UI

--- frontend/housify/src/components/Error/Error.js
+++ frontend/housify/src/components/Error/Error.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div className="text-center		bg-slate-200 h-screen	 justify-center	">
+    <div className="text-center bg-slate-200 h-screen justify-center	">
       <h1 className="text-5xl	pt-24 font-black ">Error 404</h1>
       <p className="my-6">Page not found</p>
       <Link className="text-violet-900" to="/">
