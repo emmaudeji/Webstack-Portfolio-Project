80112ef5317161545b6f30c9bf1e01b5cfc66941
Zelipha <zeliphawambui756@gmail.com>
2022-08-31 00:22:12 +0300

    Little progress on the UI

--- frontend/housify/src/App.js
+++ frontend/housify/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Property from "./components/Property/Property";
+import Agent from "./components/Agent/Agent";
+import Blog from "./components/Blog/Blog";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import { Routes, Route, Link } from "react-router-dom";
+import Error from "./components/Error/Error";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="property" element={<Property />} />
+      <Route path="agent" element={<Agent />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="blog" element={<Blog />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
 
