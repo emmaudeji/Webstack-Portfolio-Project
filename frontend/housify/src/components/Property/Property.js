d715cb86f1230859fbb0d3828dbf6a1ccf344494
gogrene <gogreneinfo@gmail.com>
2022-09-02 04:59:05 +0100

    added images to asset

--- frontend/housify/src/components/Property/Property.js
+++ frontend/housify/src/components/Property/Property.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "../Header/Header";
+import Navbar from "../Header/Navbar";
+import Hero from "../Hero/Hero";
 
 function Property() {
   return (
     <div className="Property">
-      <Header />
+      <Navbar />
+      <Hero />
     </div>
   );
 }
