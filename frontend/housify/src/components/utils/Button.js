49aaf11497b82613d1d8d2c5c69c8e09b384f65f
gogrene <gogreneinfo@gmail.com>
2022-09-02 04:39:47 +0100

    created card components

---
+++ frontend/housify/src/components/utils/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = (props) => {
+  return <button>{props.label}</button>;
+};
+
+export default Button;
---
+++ frontend/housify/src/components/utils/PropertyCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PropertyCard = (props) => {
+  return (
+    <div className="bg-zinc-800 p-4 rounded-[50px] rounded-tr-none  hover:border-2 border-zinc-400 duration-300">
+      <div className="Image-wrapper w-full bg-zinc-500 rounded-[50px] rounded-tr-none h-80 overflow-hidden mb-4">
+        <img className="w-full object-cover" src={props.img} />
+      </div>
+      <div className="px-6 pb-4 text-2xl">
+        <p className="text-right font-semibold">{props.price}</p>
+        <div class="flex justify-between pt-2">
+          <p>{props.propertyType}</p>
+          <p>
+            {props.city} <span>{props.country}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyCard;
