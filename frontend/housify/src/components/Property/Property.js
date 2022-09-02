cec11e1ff563ab51820e69bc383dc6568fa0db89
gogrene <gogreneinfo@gmail.com>
2022-09-02 05:04:33 +0100

    created the property page

--- frontend/housify/src/components/Property/Property.js
+++ frontend/housify/src/components/Property/Property.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "../Header/Navbar";
 import Hero from "../Hero/Hero";
+import FeaturedProperty from "../Property/FeaturedProperty";
+import MoreProperty from "./MoreProperty";
+import FooterEM from "../Footer/FooterEM";
 
 function Property() {
   return (
     <div className="Property">
       <Navbar />
       <Hero />
+      <FeaturedProperty />
+      <MoreProperty />
+      <FooterEM />
     </div>
   );
 }
