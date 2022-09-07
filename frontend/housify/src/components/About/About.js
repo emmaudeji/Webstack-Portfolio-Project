85cf503cbb630c5fe431d3910569a9105ea5b392
gogrene <gogreneinfo@gmail.com>
2022-09-07 18:48:52 +0100

    styled about

--- frontend/housify/src/components/About/About.js
+++ frontend/housify/src/components/About/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Header/Header";
 import Service from "./Service";
 import Footer from "../Footer/Footer";
+import SearchBar from "../utils/SearchBar";
 
 function About() {
   return (
@@ -13,6 +14,8 @@ function About() {
         text="With our vast search tool, you can find yourr dream home from any location in the world. All we do is help you find the place, meet the agents, and seal the deal. There's no extra commission for our service."
       />
 
+      <SearchBar header="Let's Find A Property That Meets Your Need" />
+      <br className=""></br>
       <Footer />
     </div>
   );
--- frontend/housify/src/components/utils/SearchBar.js
+++ frontend/housify/src/components/utils/SearchBar.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const SearchBar = (props) => {
   return (
-    <div className="w-full pt-10 px-10 md:px-[300px]">
+    <div className="w-full pt-8 px-10 md:px-[200px]">
+      <h3 className="pb-8 text-center">{props.header}</h3>
       <form action="Post">
         <div className="flex flex-col md:grid md:grid-cols-4 gap-4 p-6 md:p-4 rounded-[70px] bg-zinc-800 shadow-lg shadow-black-500 hover:border-zinc-400 hover:border-[1px] duration-300 text-center text-[22px] md:w-full">
           <div className=" p-2">
