72b0e05e257aa57173283571baf467e42468f8af
kwamboka1 <makorimildred@gmail.com>
2022-09-09 02:28:20 +0300

    [api]: Implementation

--- frontend/housify/src/components/utils/HeroImageBox.js
+++ frontend/housify/src/components/utils/HeroImageBox.js
@@ -6,8 +6,6 @@ const HeroImageBox = (props) => {
   return (
     <div className="my-auto relative pt-12">
       <div className="img-wrap overflow-hidden bg-zinc-700 rounded-[100px] rounded-tr-none w-[90%] md:ml-auto relative">
-        <img className="w-full h-[80vh] object-cover" src={House} alt="featured foto" />
-
         <img className="w-full h-[80vh] object-cover" src={props.url} alt="featured foto" />
       </div>
       <div className="absolute top-[75%]">
