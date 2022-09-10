98c9a83b66b20c1a79e210e45c27c9d04111cf08
gogrene <gogreneinfo@gmail.com>
2022-09-10 14:51:50 +0100

    edited header style

--- frontend/housify/src/components/Header/Header.js
+++ frontend/housify/src/components/Header/Header.js
@@ -14,68 +14,165 @@ function Header() {
       <div className="max-w-[1440px] mx-auto py-6 px-10 flex justify-between">
         <div>
           <a className="" href="/">
-            <img className="h-8 w-70 ml-16" src={logo} alt="Housify-logo" />
+            <img className="h-8 w-70" src={logo} alt="Housify-logo" />
           </a>
         </div>
         <div className="hidden md:flex">
           <nav className="flex items-center">
-            <NavLink to="/" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "mr-6 navlink")}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Home
             </NavLink>
-            <NavLink to="/about" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "mr-6 navlink")}>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               About
             </NavLink>
-            <NavLink to="/property" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "mr-6 navlink")}>
+            <NavLink
+              to="/property"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Property
             </NavLink>
-            <NavLink to="/agent" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "mr-6 navlink")}>
+            <NavLink
+              to="/agent"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Agent
             </NavLink>
-            <NavLink to="/contact" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "mr-6 navlink")}>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Contact
             </NavLink>
-            <NavLink to="/blog" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "mr-6 navlink")}>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Blog
             </NavLink>
 
             <button className="">
               <NavLink className="" to="/UserForm">
-                "Get Started"
+                Get Started
               </NavLink>
             </button>
           </nav>
         </div>
 
         {/* Hamburger menu */}
-        <div onClick={handleNav} className="block md:hidden mr-0 p-2 rounded-2xl hover:bg-purple-700 duration-300">
-          {nav ? <AiOutlineClose size={30} className="" /> : <AiOutlineMenu size={30} className="" />}
+        <div
+          onClick={handleNav}
+          className="block md:hidden mr-0 p-2 rounded-2xl hover:bg-purple-700 duration-300"
+        >
+          {nav ? (
+            <AiOutlineClose size={30} className="" />
+          ) : (
+            <AiOutlineMenu size={30} className="" />
+          )}
         </div>
 
         {/* Mobile Menu */}
-        <div className={nav ? "w-full left-0 flex justify-center text-center bg-zinc-900 top-[80px] absolute z-10" : "absolute left-[-100%]"}>
+        <div
+          className={
+            nav
+              ? "w-full left-0 flex justify-center text-center bg-zinc-900 top-[80px] absolute z-10"
+              : "absolute left-[-100%]"
+          }
+        >
           <nav className="pb-8">
-            <NavLink to="/" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "block p-3 text-xl navlink")}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Home
             </NavLink>
-            <NavLink to="/about" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "block p-3 text-xl navlink")}>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               About
             </NavLink>
-            <NavLink to="/property" className={({ isActive }) => (isActive ? "text-purple-600" : "block p-3 text-xl navlink")}>
+            <NavLink
+              to="/property"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Property
             </NavLink>
-            <NavLink to="/agent" className={({ isActive }) => (isActive ? "text-purple-600 mr-6" : "block p-3 text-xl navlink")}>
+            <NavLink
+              to="/agent"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Agent
             </NavLink>
-            <NavLink to="/contact" className={({ isActive }) => (isActive ? "text-purple-600" : "block p-3 text-xl navlink")}>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Contact
             </NavLink>
-            <NavLink to="/blog" className={({ isActive }) => (isActive ? "text-purple-600" : "block p-3 text-xl navlink")}>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-purple-500 block p-3 text-xl navlink"
+                  : "block p-3 text-xl navlink"
+              }
+            >
               Blog
             </NavLink>
 
             <button className="block m-4 ">
               <NavLink className="" to="/UserForm">
-                "Get Started"
+                Get Started
               </NavLink>
             </button>
           </nav>
