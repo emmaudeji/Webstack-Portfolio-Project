edd3da7bdd73b1a9f8a7c9bf6798bc6b537ef915
Zelipha <zeliphawambui756@gmail.com>
2022-09-13 21:14:30 +0300

    Add margin to sections

--- frontend/housify/src/components/Hero/Hero.js
+++ frontend/housify/src/components/Hero/Hero.js
@@ -2,23 +2,21 @@ import HeroImageBox from "../utils/HeroImageBox";
 import useFetch from "../utils/useFetch";
 
 const Hero = () => {
-
-  const {items} = useFetch()
+  const { items } = useFetch();
 
   return (
     <div className="w-full">
-      <div className="max-w-[1440px] mx-auto py-5 px-10  md:flex justify-between">
+      <div className="max-w-[1440px] mx-auto py-5 px-10 ml-16 mr-16  md:flex justify-between">
         <div className="md:grid text-center md:text-left grid-cols-2 gap-10">
           {/* Header Text */}
           <div className="my-10 md:my-auto">
             <h1 className="pb-8 text-4xl md:text-6xl">{items.header}</h1>
             {/* <p>{items.text} </p> */}
-            <p className="text-6xl mb-10">Let's help you find a home that is perfect for you</p>
+            <p className="text-6xl mb-10 font-bold">Let's help you find a home that is perfect for you</p>
           </div>
 
           {/* HeroImageBox component */}
-          <HeroImageBox p="Texas ChampHouse UK" url ={items[0]?.url}/>
-
+          <HeroImageBox p="Texas ChampHouse UK" url={items[0]?.url} />
         </div>
       </div>
     </div>
