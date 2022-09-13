62bf3ba337e4d3187565219e1e18cb0f3aabae8b
Zelipha <zeliphawambui756@gmail.com>
2022-09-13 21:07:08 +0300

    Add relevant pictures

--- frontend/housify/src/components/About/Team.js
+++ frontend/housify/src/components/About/Team.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaFacebook, FaTwitter, FaWhatsapp } from "react-icons/fa";
-import Uiface from "../../assets/44.jpg";
-import Uiface2 from "../../assets/47.jpg";
-import Uiface3 from "../../assets/79.jpg";
+import Emma from "../../assets/Emma.jpeg";
+import Milly from "../../assets/Milly.jpeg";
+import Robert from "../../assets/Robert.jpeg";
+import Zeliq from "../../assets/Zeliq.jpg";
 
 const Team = (props) => {
   return (
@@ -16,53 +17,53 @@ const Team = (props) => {
       <div className="flex flex-col justify-center  px-10 md:px-16 xl:px-[180px] sm:grid sm:grid-cols-2  lg:grid-cols-4 gap-10">
         <div className="TeamImageBox text-center">
           <div className="bg-zinc-800 rounded-full rounded-br-none w-[220px] inline-block h-[220px] mr-2 overflow-hidden">
-            <img className="w-full object-cover" src={Uiface}></img>
+            <img className="w-full object-cover" src={Zeliq} alt="Zeliq"></img>
           </div>
           <div className="inline-block">
             <h5 className="text-[24px] sm:text-[20px]">Zelipha U</h5>
             <div className="flex text-[30px]">
-              <FaFacebook className="pr-2 hover:scale-105" />
-              <FaTwitter className="pr-2 hover:scale-110" />
-              <FaWhatsapp className="pr-2 hover:scale-105" />
+              <FaFacebook className="pr-2 hover:scale-105 cursor-pointer" />
+              <FaTwitter className="pr-2 hover:scale-110 cursor-pointer" />
+              <FaWhatsapp className="pr-2 hover:scale-105 cursor-pointer" />
             </div>
           </div>
         </div>
         <div className="TeamImageBox text-center">
           <div className="bg-zinc-800 rounded-full rounded-br-none w-[220px] inline-block h-[220px] mr-2 overflow-hidden">
-            <img className="w-full object-cover" src={Uiface2}></img>
+            <img className="w-full object-cover" src={Robert} alt="Robert"></img>
           </div>
           <div className="inline-block">
             <h5 className="text-[24px] sm:text-[20px]">Rob A</h5>
             <div className="flex text-[30px]">
-              <FaFacebook className="pr-2 hover:scale-105" />
+              <FaFacebook className="pr-2 hover:scale-105 cursor-pointer" />
               <FaTwitter className="pr-2 hover:scale-110" />
-              <FaWhatsapp className="pr-2 hover:scale-105" />
+              <FaWhatsapp className="pr-2 hover:scale-105 cursor-pointer" />
             </div>
           </div>
         </div>
         <div className="TeamImageBox text-center">
           <div className="bg-zinc-800 rounded-full rounded-br-none w-[220px] inline-block h-[220px] mr-2 overflow-hidden">
-            <img className="w-full object-cover" src={Uiface3}></img>
+            <img className="w-full object-cover" src={Milly} alt="Milly"></img>
           </div>
           <div className="inline-block">
             <h5 className="text-[24px] sm:text-[20px]">Mildred A</h5>
             <div className="flex text-[30px]">
-              <FaFacebook className="pr-2 hover:scale-105" />
-              <FaTwitter className="pr-2 hover:scale-110" />
-              <FaWhatsapp className="pr-2 hover:scale-105" />
+              <FaFacebook className="pr-2 hover:scale-105 cursor-pointer" />
+              <FaTwitter className="pr-2 hover:scale-110 cursor-pointer" />
+              <FaWhatsapp className="pr-2 hover:scale-105 cursor-pointer" />
             </div>
           </div>
         </div>
         <div className="TeamImageBox text-center">
           <div className="bg-zinc-800 rounded-full rounded-br-none w-[220px] inline-block h-[220px] mr-2 overflow-hidden">
-            <img className="w-full object-cover" src={Uiface3}></img>
+            <img className="w-full object-cover" src={Emma} alt="Emma"></img>
           </div>
           <div className="inline-block">
             <h5 className="text-[24px] sm:text-[20px]">Emma A</h5>
             <div className="flex text-[30px]">
-              <FaFacebook className="pr-2 hover:scale-105" />
-              <FaTwitter className="pr-2 hover:scale-110" />
-              <FaWhatsapp className="pr-2 hover:scale-105" />
+              <FaFacebook className="pr-2 hover:scale-105 cursor-pointer" />
+              <FaTwitter className="pr-2 hover:scale-110 cursor-pointer" />
+              <FaWhatsapp className="pr-2 hover:scale-105 cursor-pointer" />
             </div>
           </div>
         </div>
