7b044276151aab2419a4947a8fb617b41d7f7f67
RobetMudzonga <robertmudzonga@yahoo.com>
2022-09-14 11:47:20 +0200

    context added

---
+++ frontend/housify/src/context/authContext/AuthContext.js
@@ -0,0 +1,10 @@
+import AuthReducer from "./AuthReducer";
+import { createContext, useEffect, useReducer } from "react";
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isFetching: false,
+  error: false,
+};
+
+export const AuthContext = createContext(INITIAL_STATE);
